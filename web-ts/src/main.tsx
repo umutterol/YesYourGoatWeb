20bd5c8e78d14bdcf6d318faa3d77cd301f6c111
umutterol <umutterol1@gmail.com>
2025-09-23 16:08:58 +0300

    ui oblitiration

--- web-ts/src/main.tsx
+++ web-ts/src/main.tsx
@@ -1,17 +1,10 @@
 import { StrictMode } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import YesYourGoat from './modes/yesyourgoat/YesYourGoat.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/yesyourgoat" element={<YesYourGoat />} />
-      </Routes>
-    </BrowserRouter>
+    <YesYourGoat />
   </StrictMode>,
 )
